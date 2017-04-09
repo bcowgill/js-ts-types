83b2e02dc3f5663f510c05558f5ecdf063cede29
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-09 01:43:36 +0100

    documented urls which helped with mocha/chai/typescript setup

--- src/types.test.ts
+++ src/types.test.ts
@@ -1,29 +1,26 @@
+import 'mocha'
+import { assert } from 'chai'
 import {
 	suite, test,
 	/* slow, timeout, skip, only */
 } from 'mocha-typescript'
 
-/*
-import chai from 'chai'
-const assert = chai.assert
-
 describe('first test suite', function() {
 	describe('.method()', function() {
 		// debugger;
 		it('should return -1 when the value is not present', function() {
-		//			assert.equal(-1, [1, 2, 3].indexOf(4));
+			assert.equal(-1, [1, 2, 3].indexOf(4));
 		});
 
 		it('should compare strings', function() {
-		//			assert.deepEqual(
-		//		['The quick brown fox jumped over the lazy dog', 'burma'],
-		//		['Dhe quik fox humpd under the dog', 'shave']);
+			assert.deepEqual(
+				['The quick brown fox jumped over the lazy dog', 'burma'],
+				['Dhe quik fox humpd under the dog', 'shave']);
 		});
 
 		it('should handle a pending test');
 	});
 });
-*/
 
 // decorator syntax for test suites
 @suite class TestPlan
