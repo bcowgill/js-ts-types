4ee9afc02e9c9481db2a55472e4cb82c18e50687
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-23 11:09:34 +0100

    test:single, cover:single tested and working
    env target cleanup
    json5 target does not entab the json files, which prevents alm editor from working

--- scripts/npmtest.sh
+++ scripts/npmtest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # test all the npm build targets we rely on
 set -x
+npm5 run env
 npm5 run json5
 npm5 run clean
 npm5 run cleanall
@@ -12,19 +13,26 @@ npm5 run compile:debug
 npm5 run compile:cover
 npm5 run test
 npm5 run testfix
-npm5 run test1 -- ./dist/string/shorten.test.js
-npm5 run test1:repl -- ./dist/string/shorten.test.js
-npm5 run test1:debug -- ./dist/string/shorten.test.js
+npm5 run test:single -- ./dist/string/shorten.test.js
+npm5 run test:single:repl -- ./dist/string/shorten.test.js
+npm5 run test:single:debug -- ./dist/string/shorten.test.js
 npm5 run cover
+npm5 run precover && npm5 run cover:single -- src/string/shorten-left.test.ts
 npm5 run coveralls
+npm5 run build
 
 exit 0
 // TODO continue testing targets here
-npm5 run build
+npm5 run cover:single -- ./src/string/shorten.test.ts
+npm5 run test:watch
+npm5 run watch-mt
+npm5 run watch-mt1
 
 exit 0
 # below can only be manually tested
 npm5 run watch
+
+// TODO continue testing targets here
 npm5 run start
 npm5 run addmodule -- module name
 npm5 run adddevmodule -- module name
